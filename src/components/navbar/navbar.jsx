e925c38d852a1e3ef174821a9a158a33c49208ff
DESKTOP-JVIJUL5\55659 <mayaramaaraujo98@gmail.com>
2021-04-20 18:52:23 -0300

    route function refactored and adjust browserrouter on appjs

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,16 +1,20 @@
 import { NavbarContainer, NavBarListItem, NavBarLogo } from '../../style/style';
 import Logo from '../../assets/logo_estrela_grande.png';
+import { useHistory } from 'react-router-dom';
+import { goToPage } from '../../utils/goToPages';
 
 function Navbar() {
+  const history = useHistory();
+
   return (
     <NavbarContainer>
-        <NavBarLogo src={Logo} alt="Logo da dupla Zé e Leandro."/>
-        <NavBarListItem href="/">home</NavBarListItem>
-        <NavBarListItem href="/photos">fotos</NavBarListItem>
-        <NavBarListItem href="/videos">vídeos</NavBarListItem>
-        <NavBarListItem href="/diary">agenda</NavBarListItem>
-        <NavBarListItem href="/biography">biografia</NavBarListItem>
-        <NavBarListItem href="/contact">contato</NavBarListItem>
+      <NavBarLogo src={Logo} alt="Logo da dupla Zé e Leandro." />      
+      <NavBarListItem onClick={() => goToPage(history, "/")}>home</NavBarListItem>
+      <NavBarListItem onClick={() => goToPage(history, "/photos")}>fotos</NavBarListItem>
+      <NavBarListItem onClick={() => goToPage(history, "/videos")}>vídeos</NavBarListItem>
+      <NavBarListItem onClick={() => goToPage(history, "/diary")}>agenda</NavBarListItem>
+      <NavBarListItem onClick={() => goToPage(history, "/biography")}>biografia</NavBarListItem>
+      <NavBarListItem onClick={() => goToPage(history, "/contact")}>contato</NavBarListItem>
     </NavbarContainer>
   )
 }
